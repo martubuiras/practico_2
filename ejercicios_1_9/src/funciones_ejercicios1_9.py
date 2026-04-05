cc0872dd6e0986a6ab1eccb3e512f664013126d3
Martina Buiras <martinabuiras@gmail.com>
2026-04-05 12:49:51 -0300

    Agrego ejercicio 2

--- ejercicios_1_9/src/funciones_ejercicios1_9.py
+++ ejercicios_1_9/src/funciones_ejercicios1_9.py
@@ -27,3 +27,45 @@ def mas_lineas_que_promedio (texto):
         if palabras_por_linea[elem] > promedio:
             print (f"{elem} ({palabras_por_linea[elem]} palabras)")
     #Imprime las lineas que tienen más palabras que el promedio (con su cantidad)
+
+def pasar_a_segundos (duracion):
+   minutos, segundos = duracion.split(":")
+   total = int(minutos) * 60 + int(segundos)
+   return total
+    #Convienrte una duración m:ss a segundos para poder operar
+
+def pasar_a_minutos (total_segundos):
+    minutos = total_segundos // 60
+    segundos = total_segundos % 60
+    return minutos, segundos
+    #Convierte una cantidad de segundos al formato m:ss para mostrar el resultado
+
+def duracion_total (playlist):
+    total = 0
+    for cancion in playlist:
+        duracion = cancion["duration"]
+        total += pasar_a_segundos(duracion)
+    return total
+    #Calcula la duracion total de la playlist sumando la duracion de cada cancion
+
+def cancion_mas_larga(playlist):
+    max_cancion = None
+    max_duracion = 0
+    for cancion in playlist:
+        dur = pasar_a_segundos(cancion["duration"])
+        if dur > max_duracion:
+            max_duracion = dur
+            max_cancion = cancion
+    return max_cancion
+    #Devuelve la cancion mas larga de una playlist
+
+def cancion_mas_corta(playlist):
+    min_cancion = None
+    min_duracion = float('inf')
+    for cancion in playlist:
+        dur = pasar_a_segundos(cancion["duration"])
+        if dur < min_duracion:
+            min_duracion = dur
+            min_cancion = cancion
+    return min_cancion
+    #Devuelve la cancion mas corta de una playlist
